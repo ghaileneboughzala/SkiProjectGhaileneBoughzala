7bf8601bf201e6b797e57b72c88fa6659cf15513
ghaileneboughzala <ghailene.boughzala@esprit.tn>
2023-04-18 10:53:50 +0100

    Update ExceptionHandler.java

--- src/main/java/com/example/skighailene/aspects/ExceptionHandler.java
+++ src/main/java/com/example/skighailene/aspects/ExceptionHandler.java
@@ -2,6 +2,12 @@ package com.example.skighailene.aspects;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+
 @RestControllerAdvice
 public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public Map<String, String> HandleException(Exception exception){
+
+    }
 }
