594b454c8c44c155dc6a576ddc524f0b302bef64
ghaileneboughzala <ghailene.boughzala@esprit.tn>
2023-04-18 10:57:07 +0100

    Update Exception

--- src/main/java/com/example/skighailene/aspects/ExceptionHandler.java
+++ src/main/java/com/example/skighailene/aspects/ExceptionHandler.java
@@ -2,12 +2,17 @@ package com.example.skighailene.aspects;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler
     public Map<String, String> HandleException(Exception exception){
+
+        Map map = new HashMap<>();
+        map.put("error", exception.getMessage());
+        return map;
 
     }
 }
