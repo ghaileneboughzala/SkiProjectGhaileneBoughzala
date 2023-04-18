1757ab5aa1273c5adb194f44dea5ccad66bc11a4
ghaileneboughzala <ghailene.boughzala@esprit.tn>
2023-04-18 11:02:21 +0100

    Final Exception Handler

--- src/main/java/com/example/skighailene/aspects/ExceptionHandler.java
+++ src/main/java/com/example/skighailene/aspects/ExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.example.skighailene.aspects;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestControllerAdvice
 public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public Map<String, String> HandleException(Exception exception){
 
         Map map = new HashMap<>();
